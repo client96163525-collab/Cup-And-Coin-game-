8699ee167180c62f0b41108152cd9215e494ec6c
Abhi <client96163525@gmail.com>
2026-08-21 18:18:12 +0530

    hi

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     applicationId = "com.abhix.cupandcoin"
     minSdk = 24
     targetSdk = 36
-    versionCode = 1
-    versionName = "1.0"
+    versionCode = 3
+    versionName = "1.0.3"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
